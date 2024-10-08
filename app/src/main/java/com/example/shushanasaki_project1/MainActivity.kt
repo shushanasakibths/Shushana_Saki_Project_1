e2370ea7a93f333d51d528bdcb6e533866b2047e
BT_1N3_33 <sample@bths.edu>
2024-10-08 08:45:19 -0400

    Day 1 p2: Layout complete, variables created, few methods started

--- app/src/main/java/com/example/shushanasaki_project1/MainActivity.kt
+++ app/src/main/java/com/example/shushanasaki_project1/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.shushanasaki_project1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var scoreCount: Int = 0
+    private var strikeCount: Int = 0
+    private lateinit var startButton: Button
+    private lateinit var firstBox: TextView
+    private lateinit var secondBox: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.start_button)
+        firstBox = findViewById(R.id.first_box)
+        secondBox = findViewById(R.id.second_box)
+
+    }
+
+    private fun randNum() {
+        firstBox.text = (Random.nextInt(100) + 1).toString()
+        secondBox.text = (Random.nextInt(100) + 1).toString()
+    }
+
+    private fun checkLargerNum() {
+
     }
 }
